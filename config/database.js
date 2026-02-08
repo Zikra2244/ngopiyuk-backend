56842886584081e2787b61563eeb96e40e625f88
Zikra2244 <zikragaul12321@gmail.com>
2026-02-09 04:00:01 +0700

    Fix: Rebuild package-lock to include pg driver 2

--- config/database.js
+++ config/database.js
@@ -1,19 +1,18 @@
+// config/database.js
+const pg = require("pg"); // TAMBAHKAN INI UNTUK MEMAKSA BUNDLER VERCEL
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: pg, // BERITAHU SEQUELIZE UNTUK MENGGUNAKAN MODULE INI
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
+    prepareThreshold: 0,
   },
-  // PENTING: Tambahkan baris ini untuk mendukung Transaction Pooler Supabase
-  minifyAliases: true,
-  // Disable prepared statements
-  prepareThreshold: 0,
-  logging: false,
 });
 
 module.exports = sequelize;
