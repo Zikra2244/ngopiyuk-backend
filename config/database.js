7d17de5154f72a8f142dfaef7b3e8e963901c85f
Zikra2244 <zikragaul12321@gmail.com>
2026-02-09 02:01:25 +0700

    Vercel Deployment 1

--- config/database.js
+++ config/database.js
@@ -1,23 +1,16 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-// Inisialisasi Sequelize dengan SSL untuk Supabase
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+// Menggunakan DATABASE_URL yang lebih simpel dan handal untuk Vercel
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
   },
-);
+});
 
 module.exports = sequelize;
